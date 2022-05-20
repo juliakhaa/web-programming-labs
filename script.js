d7e5e18cf82a972b08dd3e84878023bdfe790d30
Юлия Харитоненко <kroojulia@gmail.com>
2022-05-20 21:06:43 +0700

    Генерация меню

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function notReadyAlert() {
+function notReadyAlert(event) {
     alert('Sorry, not ready yet!\nИзвините, еще не готово!');
     console.log('Sorry, not ready yet!\nИзвините, еще не готово!');
+    event.preventDefault();
     return false;    
 }
 
@@ -57,10 +58,39 @@ function search() {
     }, 2000);
 }
 
+function generateMenu() {
+    let menu = document.querySelector('nav.main-menu ul');
+    menu.innerHTML = '';
+
+    let items = [
+        {href: 'index.html', text: 'Товары'},
+        {href: '', text: 'Контакты'},
+        {href: '', text: 'Доставка'},
+        {href: '', text: 'Акции'},
+        {href: '', text: 'О нас'},
+    ];
+
+    for(let i = 0; i<items.length; i++) {
+        let link = document.createElement('a');
+        link.innerText = items[i].text;
+        link.href = items[i].href;
+        if(items[i].href == '') {
+            link.addEventListener('click', notReadyAlert);
+        }
+
+        let menuItem = document.createElement('li');
+        menuItem.appendChild(link);
+
+        menu.appendChild(menuItem);
+    }
+}
+
 function loaded() {
     let searchbox = document.getElementById('search');
     searchbox.addEventListener('keydown', function (key) {
         if(key.key == 'Enter')
         search();
     });
+
+    generateMenu();
 }
